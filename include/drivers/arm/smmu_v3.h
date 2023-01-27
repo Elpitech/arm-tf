9f968702de9c1549c0fc3d8c9aab33619c47e166
Vadim V. Vlasov <vadim.vlasov@elpitech.ru>
2023-01-27 15:13:07 +0300

    Baikal patch to common smmu_v3 code

--- include/drivers/arm/smmu_v3.h
+++ include/drivers/arm/smmu_v3.h
@@ -12,12 +12,26 @@
 #include <platform_def.h>
 
 /* SMMUv3 register offsets from device base */
-#define SMMU_CR0	U(0x0020)
-#define SMMU_CR0ACK	U(0x0024)
-#define SMMU_GBPA	U(0x0044)
-#define SMMU_S_IDR1	U(0x8004)
-#define SMMU_S_INIT	U(0x803c)
-#define SMMU_S_GBPA	U(0x8044)
+#define SMMU_IDR0		U(0x0000)
+#define SMMU_IDR1		U(0x0004)
+#define SMMU_CR0		U(0x0020)
+#define SMMU_CR0ACK		U(0x0024)
+#define SMMU_GBPA		U(0x0044)
+#define SMMU_S_IDR0		U(0x8000)
+#define SMMU_S_IDR1		U(0x8004)
+#define SMMU_S_CR0		U(0x8020)
+#define SMMU_S_CR0ACK		U(0x8024)
+#define SMMU_S_CR1		U(0x8028)
+#define SMMU_S_INIT		U(0x803c)
+#define SMMU_S_GBPA		U(0x8044)
+#define SMMU_S_STRTAB_BASE	U(0x8080)
+#define SMMU_S_STRTAB_BASE_CFG	U(0x8088)
+#define SMMU_S_CMDQ_BASE	U(0x8090)
+#define SMMU_S_CMDQ_PROD	U(0x8098)
+#define SMMU_S_CMDQ_CONS	U(0x809c)
+#define SMMU_S_EVENTQ_BASE	U(0x80a0)
+#define SMMU_S_EVENTQ_PROD	U(0x80a8)
+#define SMMU_S_EVENTQ_CONS	U(0x80ac)
 
 /*
  * TODO: SMMU_ROOT_PAGE_OFFSET is platform specific.
@@ -41,6 +55,8 @@
 
 /* SMMU_CR0 and SMMU_CR0ACK register fields */
 #define SMMU_CR0_SMMUEN			(1UL << 0)
+#define SMMU_CR0_EVENTQEN		(1UL << 2)
+#define SMMU_CR0_CMDQEN			(1UL << 3)
 
 /* SMMU_GBPA register fields */
 #define SMMU_GBPA_UPDATE		(1UL << 31)
@@ -63,9 +79,47 @@
 #define SMMU_ROOT_CR0_GPCEN		(1UL << 1)
 #define SMMU_ROOT_CR0_ACCESSEN		(1UL << 0)
 
+/* STE definitions */
+#define SMMU_STE_SIZE			64
+#define SMMU_STE_0_V			(1UL << 0)
+#define SMMU_STE_0_CONFIG_MASK		GENMASK(3, 1)
+#define SMMU_STE_0_CONFIG_SHIFT		1
+#define SMMU_STE_3_MEMATTR_MASK		GENMASK(3, 0)
+#define SMMU_STE_3_MEMATTR_SHIFT	0
+#define SMMU_STE_3_MTCFG		(1UL << 4)
+#define SMMU_STE_3_ALLOCCFG_MASK	GENMASK(8, 5)
+#define SMMU_STE_3_ALLOCCFG_SHIFT	5
+#define SMMU_STE_3_SHCFG_MASK		GENMASK(13, 12)
+#define SMMU_STE_3_SHCFG_SHIFT		12
+#define SMMU_STE_3_NSCFG_MASK		GENMASK(15, 14)
+#define SMMU_STE_3_NSCFG_SHIFT		14
+#define SMMU_STE_3_PRIVCFG_MASK		GENMASK(17, 16)
+#define SMMU_STE_3_PRIVCFG_SHIFT	16
+#define SMMU_STE_3_INSTCFG_MASK		GENMASK(19, 18)
+#define SMMU_STE_3_INSTCFG_SHIFT	18
+
+#define SMMU_STE_CONFIG_BYPASS_BYPASS	4
+#define SMMU_STE_MEMATTR_IWB_OWB	0xf
+#define SMMU_STE_MTCFG_USE_INCOMING	0
+#define SMMU_STE_MTCFG_REPLACE_MEMATTR	1
+#define SMMU_STE_ALLOCCFG_USE_INCOMING	0
+#define SMMU_STE_ALLOCCFG_RW		0xe
+#define SMMU_STE_SHCFG_USE_INCOMING	1
+#define SMMU_STE_SHCFG_OUTER		2
+#define SMMU_STE_NSCFG_USE_INCOMING	0
+#define SMMU_STE_NSCFG_NSEC		3
+#define SMMU_STE_PRIVCFG_USE_INCOMING	0
+#define SMMU_STE_INSTCFG_USE_INCOMING	0
+
 int smmuv3_init(uintptr_t smmu_base);
 int smmuv3_security_init(uintptr_t smmu_base);
 
 int smmuv3_ns_set_abort_all(uintptr_t smmu_base);
+
+int smmuv3_enable_queues(uintptr_t smmu_base, bool sec);
+
+int smmuv3_invalidate_all(uintptr_t smmu_base);
+
+int smmuv3_enable(uintptr_t smmu_base, bool sec);
 
 #endif /* SMMU_V3_H */

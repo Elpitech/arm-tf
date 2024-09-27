5be41d02afb75c1ce321bc83cf1f20de696bc07b
Vadim V. Vlasov <vadim.vlasov@elpitech.ru>
2024-09-27 18:26:20 +0300

    Baikal-M: smbus: Return -1 on TX error
    This allows to distinguish TX error from 0 bytes received.
    0 bytes received is not an error in case of tx-only request.

--- plat/baikal/bm1000/drivers/bm1000_smbus.h
+++ plat/baikal/bm1000/drivers/bm1000_smbus.h
@@ -12,14 +12,14 @@ enum smbus_sht {
 	SMBUS_SHT_100KHZ = 1
 };
 
-unsigned int smbus_txrx(uintptr_t base,
-			unsigned int iclk,
-			enum smbus_sht sht,
-			unsigned int sclclk,
-			unsigned int targetaddr,
-			const void *txbuf,
-			unsigned int txbufsize,
-			void *rxbuf,
-			unsigned int rxbufsize);
+int smbus_txrx(uintptr_t base,
+	       unsigned int iclk,
+	       enum smbus_sht sht,
+	       unsigned int sclclk,
+	       unsigned int targetaddr,
+	       const void *txbuf,
+	       unsigned int txbufsize,
+	       void *rxbuf,
+	       unsigned int rxbufsize);
 
 #endif /* BM1000_SMBUS_H */
